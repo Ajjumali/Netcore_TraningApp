f9d0981a2c5d932e7fa254e721d30454cd7d733b
Ajjumali <93416931+Ajjumali@users.noreply.github.com>
2022-09-28 13:07:18 +0530

    Add files via upload

---
+++ MyConnection.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TrainingApp.Models;
+
+namespace TrainingApp
+{
+    public class MyConnection: DbContext
+    {
+        public MyConnection()
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=172.16.41.226;Database=traningdb;User ID=CMS; password=jgc#2018$Dev");
+        }
+
+        public DbSet<PCWPS> PCWBS { get; set; }
+        public DbSet<FWBS> Fwbs { get; set; }
+        public DbSet<MileStone> MileStone { get; set; }
+
+        
+    }
+}
